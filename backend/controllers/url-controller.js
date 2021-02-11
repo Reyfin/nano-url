3451e6c1576f7fc497bfc07c547c0333615bc610
Devanshu Mevada <devanshumevada@gmail.com>
2021-02-11 18:12:37 +0530

    Added Memcachier support for getting unique index

--- backend/controllers/url-controller.js
+++ backend/controllers/url-controller.js
@@ -1,9 +1,19 @@
 const util = require('util');
 const urlExists = util.promisify(require('url-exists'));
+const MEMC_USERNAME = require("../config/default.json").MEMC_USERNAME;
+const MEMC_PASSWORD = require("../config/default.json").MEMC_PASSWORD;
+const MEMC_SERVER = require("../config/default.json").MEMC_SERVER;
+
 
 const urlModel = require("../models/url-model");
 const id_to_base62 = require("../algorithm/conversionAlgo");
 const nanourl = require("../config/default.json").Shortened_URI;
+
+const memjs  = require('memjs');
+const mc = new memjs.Client.create(MEMC_SERVER, {
+  username: MEMC_USERNAME,
+  password: MEMC_PASSWORD
+});
 
 exports.getBasePage = (req, res, next) => {
   urlModel
@@ -22,7 +32,14 @@ exports.getBasePage = (req, res, next) => {
 
 exports.postShortUrl = async (req, res, next) => {
   const { longUrl } = req.body;
-  const shortCode = id_to_base62.id_to_base62(longUrl.length);
+  const index = await mc.get('index');
+    
+  
+  const new_index = JSON.parse( await index.value) + 1;
+  await mc.set('index', `${new_index}`);
+  console.log(new_index)
+  
+  const shortCode = id_to_base62.id_to_base62(new_index);
 
   let urlIsValid = await urlExists(longUrl);
   
